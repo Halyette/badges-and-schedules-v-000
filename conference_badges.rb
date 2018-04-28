e864103831df0200286ccd63ea09b70f9057cad3
Halyette <haly.lesane@gmail.com>
2018-04-28 20:14:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,31 @@
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
+badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
-  "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.map do |attendee|
-    "Hello, my name is #{attendee}."
+def batch_badge_creator(name)
+  names = []
+  names.each do |attendees|
+    names <<  "Hello, my name is #{badges}."
   end
+  names
 end
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+def assign_rooms(name)
+  names = []
+  counter = 1
+  badge.each do |attendees|
+    badges <<  "Hello, #{attendees}! You'll be assigned to room #{counter}!"
+    counter += 1
   end
+  badges
 end
 
-def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+def printer(badge)
+  batch_badge_creator(badge).each do |badges|
+    puts "#{badges}"
   end
-
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
+  assign_rooms(badge).each do |badges|
+    puts "#{badges}"
   end
 end
